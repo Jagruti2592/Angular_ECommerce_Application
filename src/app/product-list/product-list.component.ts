b317a7168af92ac311a091f7de10db2e1962653c
Jagruti Chitte <81913740+Jagruti2592@users.noreply.github.com>
2024-09-22 12:20:36 -0400

    event Binding / product-list

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,14 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  name = 'John Doe';
+  
   product={
     name :'iphone13',
     price: 999,
     color:'Red',
-    discount:8.5
+    discount:8.5,
+    inStock: 5,
+    pImage : "angular-ekart/src/assets/images/dog1.jpeg"
   }
 
   getDiscountedPrice(){
     return this.product.price - (this.product.price * this.product.discount) / 100;
   }
+  
+  onNameChange(event: any){
+    this.name = event.target.value;
+    console.log(this.name);
+  }
+  
 }
