700cd61c036aff82211e035733e2ace243e7e8fc
Jagruti Chitte <81913740+Jagruti2592@users.noreply.github.com>
2024-09-23 12:36:57 -0400

    Structural Directive example code added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,42 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  name = 'John Doe';
-  addToCart: number = 0;
-  product={
-    name :'iphone13',
-    price: 999,
-    color:'Red',
-    discount:8.5,
-    inStock: 5,
-    pImage : "angular-ekart/src/assets/images/dog1.jpeg"
-  }
+  // name = 'John Doe';
+  // addToCart: number = 0;
+  // product={
+  //   name :'iphone13',
+  //   price: 999,
+  //   color:'Red',
+  //   discount:8.5,
+  //   inStock: 5,
+  //   pImage : "angular-ekart/src/assets/images/dog1.jpeg"
+  // }
 
-  getDiscountedPrice(){
-    return this.product.price - (this.product.price * this.product.discount) / 100;
-  }
+  // getDiscountedPrice(){
+  //   return this.product.price - (this.product.price * this.product.discount) / 100;
+  // }
   
-  onNameChange(event: any){
-    this.name = event.target.value;
-    console.log(this.name);
-  }
+  // onNameChange(event: any){
+  //   this.name = event.target.value;
+  //   console.log(this.name);
+  // }
 
-  decrementCartValue(){
-    if(this.addToCart > 0)
-      this.addToCart--;
-    else
-      this.addToCart = 0;
+  // decrementCartValue(){
+  //   if(this.addToCart > 0)
+  //     this.addToCart--;
+  //   else
+  //     this.addToCart = 0;
     
-    }
+  //   }
   
 
-  incrementCartValue(){ 
-    if(this.addToCart < this.product.inStock)   
-      this.addToCart++;
-    else
-      this.addToCart = this.product.inStock;
+  // incrementCartValue(){ 
+  //   if(this.addToCart < this.product.inStock)   
+  //     this.addToCart++;
+  //   else
+  //     this.addToCart = this.product.inStock;
     
-    }
+  //   }
   
   
 }
